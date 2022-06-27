7b6b263cd732b70340c6ed1561c0ce7e9cec1571
fraz <fraz@o2.pl>
2022-06-27 17:01:34 +0200

    connected to backend api, added styled-components support in next

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,44 @@
 import React from 'react'
 import styled from 'styled-components'
 import MESSAGES from '../messages/messages'
+import { FETCH_CATEGORIES } from '../utils/api'
 
 const Header = styled.h1.attrs({
     className: 'text-5xl font-bold',
 })``
 
-const Home: React.FC = () => (
+interface Category {
+    name: string
+    id: number
+}
+
+const Home: React.FC<{ categories: [Category] }> = ({ categories }) => (
     <div className='hero min-h-screen bg-base-200'>
-        <div className='hero-content text-center'>
-            <div className='max-w-md'>
-                <Header>{MESSAGES.global.appName}</Header>
-                <button
-                    className='btn btn-primary mt-6'
-                    id='sss'>
-                    {MESSAGES.index.action}
-                </button>
+        <div className='hero-content text-center flex-col'>
+            <Header>{MESSAGES.global.appName}</Header>
+            <div className='max-w-xl grid grid-cols-3 gap-5'>
+                {categories.map((cat: Category) => (
+                    <div key={cat.id}>
+                        <h2 className='card-title'>{cat.name}</h2>
+                    </div>
+                ))}
             </div>
+            <button className='btn btn-primary mt-6'>
+                {MESSAGES.index.action}
+            </button>
         </div>
     </div>
 )
+
+export async function getStaticProps() {
+    const respone = await fetch(FETCH_CATEGORIES)
+    const categories: [Category] = await respone.json()
+
+    return {
+        props: {
+            categories,
+        },
+    }
+}
 
 export default Home
---
+++ utils/api.ts
@@ -0,0 +1,3 @@
+export const API_URL:string = 'https://x8ki-letl-twmt.n7.xano.io/api:fccO_oAf'
+
+export const FETCH_CATEGORIES:string = `${API_URL}/category`
