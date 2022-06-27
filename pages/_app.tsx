34dcaa008180535035bfe392e55837f50328ffbd
fraz <fraz@o2.pl>
2022-06-27 18:16:27 +0200

    added global document title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import MESSAGES from '../messages/messages'
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <title>{MESSAGES.global.appName}</title>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default MyApp
