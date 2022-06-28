bbc7b8b6d2a06806743fb7289f5b4fe0af393c05
fraz <fraz@o2.pl>
2022-06-28 11:52:31 +0200

    added fetcher function, fixed text wrapping

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import MESSAGES from '../messages/messages'
-import { FETCH_CATEGORIES } from '../utils/api'
+import { FETCH_CATEGORIES, getFromApi } from '../utils/api'
 
 const Header = styled.h1.attrs({
     className: 'text-5xl font-bold',
@@ -12,14 +12,18 @@ interface Category {
     id: number
 }
 
-const Home: React.FC<{ categories: [Category] }> = ({ categories }) => (
+type Categories = [Category]
+
+const Home: React.FC<{ categories: Categories }> = ({ categories }) => (
     <div className='hero min-h-screen bg-base-200'>
         <div className='hero-content text-center flex-col'>
             <Header>{MESSAGES.global.appName}</Header>
             <div className='max-w-xl grid grid-cols-3 gap-5'>
                 {categories.map(({ id, name }) => (
                     <div key={`item-${id}`}>
-                        <h2 className='card-title'>{name}</h2>
+                        <h2 className='card-title truncate block text-left'>
+                            {name}
+                        </h2>
                     </div>
                 ))}
             </div>
@@ -31,8 +35,7 @@ const Home: React.FC<{ categories: [Category] }> = ({ categories }) => (
 )
 
 export async function getStaticProps() {
-    const respone = await fetch(FETCH_CATEGORIES)
-    const categories: [Category] = await respone.json()
+    const categories = await getFromApi<Categories>(FETCH_CATEGORIES)
 
     return {
         props: {
--- utils/api.ts
+++ utils/api.ts
@@ -1,3 +1,8 @@
 export const API_URL:string = 'https://x8ki-letl-twmt.n7.xano.io/api:fccO_oAf'
 
 export const FETCH_CATEGORIES:string = `${API_URL}/category`
+
+export async function getFromApi<T>(url: string): Promise<T> {
+    const response = await fetch(url)
+    return await response.json()
+}
